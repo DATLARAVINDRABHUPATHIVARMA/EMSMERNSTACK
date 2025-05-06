d42ae6300bba92453bcb8ded3d8d7ca51ddc1e01
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-05-06 11:43:25 +0530

    Adding specific fields of employee in backend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+
+  return (
+    <div className="text-3xl text-green-600">Welcome TO EMS of Seven Hills Facility Services Privated Limited
+    </div>
+  )
+}
+
+export default App
