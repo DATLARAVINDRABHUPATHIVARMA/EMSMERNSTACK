e656d2df8a62375615cdd7f202961892e897b681
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-05-07 18:00:23 +0530

    Tailwind 4 version

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -2,20 +2,22 @@ import React from "react";
 
 const Login = () => {
     return(
-        <div>
-            <h1> Employee Management System </h1>
-            <form>
-                <h2>Login</h2>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" placeholder="Enter Email"/>
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" placeholder="********"/>
-                </div>
+        <div className="flex flex-col items-center h-screen justify-center bg-gradient-to-b from-green-600 from-50% to-gray-100 to-50% space-y-6">
+            <h2 className="font-Sevillana text-3xl text-white"> Employee Management System </h2>
+            <div className="border shadow p-6 w-80 bg-white">
+                <h2 className="text-2xl font-bold mb-4">Login</h2>
+                <form>
+                    <div className="mb-4">
+                        <label htmlFor="email" className="block text-grey-700">Email</label>
+                        <input type="email" placeholder="Enter Email"/>
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" placeholder="********"/>
+                    </div>
+                </form>
                 <button>Login</button>
-            </form>
+            </div>
         </div>
     )
 }
