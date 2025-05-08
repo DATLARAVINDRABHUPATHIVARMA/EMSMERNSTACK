9856bcecca77402a8cdafda43d7f4fe4dc41acf4
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-05-08 18:16:47 +0530

    Bugs in clientside console

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -1,24 +1,24 @@
 import axios from "axios";
-import React from "react";
+import React, {useState} from "react";
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
 
     const handleSubmit = async (e) => {
         e.preventDefault()
         try {
-            const response = await axios.post('http://localhost:2000/api/auth/login', {email, password}
+            const response = await axios.post('http://localhost:5000/api/auth/login', {email, password}
             );
             console.log(response);
-            
         } catch (error) {
             console.log(error)
         }
     }
     return(
         <div className="flex flex-col items-center h-screen justify-center bg-gradient-to-b from-green-600 from-50% to-gray-100 to-50% space-y-6">
-            <h2 className="font-sevillana text-3xl text-white"><image/> Employee Management System </h2>
+            <h2 className="font-sevillana text-3xl text-white"> Employee Management System </h2>
             <div className="border shadow p-6 w-80 bg-white">
                 <h2 className="text-2xl font-bold mb-4">Login</h2>
                 <form onSubmit={handleSubmit}>
--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 import authRouter from './routes/auth.js'
+import connectToDatabase from './DB/db.js'
 
+connectToDatabase()
 const app = express()
 app.use(cors())
 app.use(express.json())
