c4072a562e9e233a0855a9d7185fc7a84ff04efc
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2025-05-09 12:44:30 +0530

    upto DB registration

---
+++ server/userSeed.js
@@ -0,0 +1,22 @@
+import User from "./models/User.js";
+import bcrypt from "bcrypt";
+import connectToDatabase from "./db/db.js";
+
+const userRegister = async () => {
+  connectToDatabase();
+  try{
+    const hashPassword = await bcrypt.hash("admin", 10);
+    const newUser = new User({
+      name: "John Doe",
+      contact: "1234567890",
+      email: "johndoe@example.com",
+      password: hashPassword,
+      role: "ADMIN"
+    })
+    await newUser.save();
+  } catch(error){
+    console.log(error);
+  }
+}
+
+userRegister();
