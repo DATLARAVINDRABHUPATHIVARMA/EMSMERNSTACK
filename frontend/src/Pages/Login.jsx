70010f8994c5ee48e3522032f9f5e463e1e405af
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-05-09 18:25:44 +0530

    login successful

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -4,14 +4,21 @@ import React, { useState } from "react";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const response = await axios.post("http://localhost:5000/api/auth/login", { email, password });
-      console.log(response)
+      if (response.data.success) {
+        alert("Login Successful!");
+      }
     } catch (error) {
-      console.log(error);
+      if(error.response && !error.response.data.success) {
+        setError(error.response.data.error);
+      } else{
+        setError("Server Error");
+      }
     }
   };
 
@@ -22,6 +29,7 @@ const Login = () => {
       </h2>
       <div className="border shadow p-6 w-80 bg-white">
         <h2 className="text-2xl font-bold mb-4">Login</h2>
+        {error && <p className="text-red-500">{error}</p>}
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label htmlFor="email" className="block text-gray-700">
