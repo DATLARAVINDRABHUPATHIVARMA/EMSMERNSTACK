b967f763f03b6acccef5679e01c13c2229048552
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2025-05-12 15:32:10 +0530

    checking backend bug for authMiddleware

---
+++ frontend/src/Pages/AdminDashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useAuth } from '../context/authContext.jsx'
+
+const AdminDashboard = () => {
+  const {user} = useAuth()
+  return (
+    <div>
+      AdminDashboard {user && user.name}
+    </div>
+  )
+}
+
+export default AdminDashboard
