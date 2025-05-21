a4adbf14737bba10488f4e9467536a6cb1346fdc
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-05-21 16:54:30 +0530

    No error in frontend for add dept need to check backend

--- frontend/src/Components/Department/AddDepartment.jsx
+++ frontend/src/Components/Department/AddDepartment.jsx
@@ -8,7 +8,7 @@ const AddDepartment = () => {
     description: "",
   });
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -20,69 +20,65 @@ const AddDepartment = () => {
     try {
       const response = await axios.post("http://localhost:5000/api/department/add", department, {
         headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
+          "Authorization": `Bearer ${localStorage.getItem('token')}`
+        }
       });
-      if (response.data.success) {
+      if(response.data.success){
         navigate('/admin-dashboard/departments')
-        alert("Department added successfully");
-        setDepartment({ dep_name: "", description: "" });
-      } else {
-        alert(response.data.error);
       }
     } catch (error) {
       if (error.response && !error.response.data.success) {
         alert(error.response.data.error);
       }
     }
-
-    return (
-      <div className="max-w-3xl mx-auto mt-10 bg-white p-8 rounded-md shadow-md w-96">
-        <h2 className="text-2xl font-bold mb-6">Add New Department</h2>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label
-              htmlFor="dep_name"
-              className="text-sm font-medium text-gray-700"
-            >
-              Department Name
-            </label>
-            <input
-              type="text"
-              name="dep_name"
-              onChange={handleChange}
-              placeholder="Department Name"
-              className="mt-1 w-full p-2 border border-gray-300 rounded-md"
-              required
-            />
-          </div>
-
-          <div className="mt-3">
-            <label
-              htmlFor="description"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Description
-            </label>
-            <textarea
-              name="description"
-              onChange={handleChange}
-              placeholder="Description"
-              className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
-              rows="5"
-            />
-          </div>
-
-          <button
-            type="submit"
-            className="w-full mt-6 bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded"
-          >
-            Add Department
-          </button>
-        </form>
-      </div>
-    );
   };
+
+  return (
+    <div className="max-w-3xl mx-auto mt-10 bg-white p-8 rounded-md shadow-md w-96">
+      <h2 className="text-2xl font-bold mb-6">Add New Department</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label
+            htmlFor="dep_name"
+            className="text-sm font-medium text-gray-700"
+          >
+            Department Name
+          </label>
+          <input
+            type="text"
+            name="dep_name"
+            onChange={handleChange}
+            placeholder="Department Name"
+            className="mt-1 w-full p-2 border border-gray-300 rounded-md"
+            required
+          />
+        </div>
+
+        <div className="mt-3">
+          <label
+            htmlFor="description"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Description
+          </label>
+          <textarea
+            name="description"
+            onChange={handleChange}
+            placeholder="Description"
+            className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+            rows="5"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-full mt-6 bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded"
+        >
+          Add Department
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default AddDepartment;
