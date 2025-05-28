f466c6293f91e2346037ef43ad370e03fb812c8c
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2025-05-29 00:12:50 +0530

    42 min need to check morning

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import cors from 'cors'
 import authRouter from './routes/auth.js'
+import connectToDatabase from "./db/db.js"
 
 
+connectToDatabase()
 const app = express()
 app.use(cors())
 app.use(express.json())
