6ff20aa24ae4224eb0022d20c486149f588d84cb
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2025-05-29 12:18:28 +0530

    begin 2 video done in writing code need to test

--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -1,9 +1,42 @@
-import React, { createContext, useState, useContext } from "react";
+import axios from "axios";
+import React, { createContext, useState, useEffect } from "react";
 
 const userContext = createContext();
 
 const authContext = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const verifyUser = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        if (token) {
+          const response = await axios.get(
+            "http://localhost:5000/api/auth/verify",
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          if (response.data.success) {
+            setUser(response.data.user);
+          }
+        } else {
+          setUser(null);
+        }
+      } catch (error) {
+        console.log(error);
+        if (error.response && !error.response.data.error) {
+          setUser(null);
+        }
+      } finally {
+          setLoading(false)
+      }
+    };
+    verifyUser();
+  }, []);
 
   const login = (user) => {
     setUser(user);
@@ -14,7 +47,7 @@ const authContext = ({ children }) => {
     localStorage.removeItem("token");
   };
   return (
-    <userContext.Provider value={{ user, login, logout }}>
+    <userContext.Provider value={{ user, login, logout, loading }}>
       {children}
     </userContext.Provider>
   );
--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { useAuth } from '../context/authContext.jsx'
+import { useNavigate } from 'react-router-dom'
 
 const AdminDashboard = () => {
-  const {user} = useAuth()
+  const {user, loading} = useAuth()
+  const navigate = useNavigate()
+
+  if(loading){
+    return <div>Loading ...</div>
+  }
+  if(!user){
+    navigate('/login')
+  }
   return (
     <div>
-      Admin Dashboard of {user.name}
+      Admin Dashboard of {user && user.name}
     </div>
   )
 }
