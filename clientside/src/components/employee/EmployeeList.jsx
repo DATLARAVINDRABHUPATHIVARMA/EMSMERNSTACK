711f1434b2a777e01c562a292a62b417c3b9245b
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2025-06-02 10:24:38 +0530

    emp list 06:00 5th

--- clientside/src/components/employee/EmployeeList.jsx
+++ clientside/src/components/employee/EmployeeList.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom'
 
 const EmployeeList = () => {
   return (
-    <div className='p-5'>
+    <div className="p-5">
       <div className="text-center">
         <h3 className="text-2xl font-bold">Manage Employees</h3>
       </div>
       <div className="flex justify-between items-center">
         <input type="text" placeholder="Search By Employee ID" className="px-4 py-0.5 border"
-        // onChange={filterDepartments}
+        // onChange={filterClients}
         />
-        <Link to="/admin-dashboard/add-Employee" className="px-4 py-1 bg-purple-500 rounded text-white">
+        <Link to="/admin-dashboard/add-employee" className="px-4 py-1 bg-purple-500 rounded text-white">
           Add New Employee
         </Link>
       </div>
