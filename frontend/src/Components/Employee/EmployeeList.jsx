8024061c26a44c448c6c7f9ad37045cb458a209f
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-06-03 18:10:36 +0530

    employee display on section doing

--- frontend/src/Components/Employee/EmployeeList.jsx
+++ frontend/src/Components/Employee/EmployeeList.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const EmployeeList = () => {
+  const [employees, setEmployees] = useState([]);
+  const [employeeLoading, setEmployeeLoading] = useState(false);
+
+  useEffect(() => {
+      const fetchEmployees = async () => {
+        setEmployeeLoading(true);
+        try {
+          const response = await axios.get("http://localhost:5000/api/employee",
+            {
+              headers: {'Authorization': `Bearer ${localStorage.getItem("token")}`,},
+            }
+          );
+          if(response.data.success){
+            let sno = 1;
+            const data = await response.data.employees.map((employee) => ({
+              _id: employee._id,
+              sno: sno++,
+              employeeID: employee.employeeID,
+              profileImage: employee.userId.profileImage,
+              name: employee.userId.name,
+              departmentName: employee.department.departmentName,
+              designation: employee.designation,
+              action: (
+              ),
+            }))
+            setEmployees(data);
+            setFilteredDepartments(data)
+          }
+        } catch(error){
+          if (error.response && !error.response.data.success) {
+            alert(error.response.data.error);
+          }
+        } finally {
+          setEmployeeLoading(false);
+        }
+      }
+  
+      fetchEmployees();
+    }, [])
+
   return (
     <div className="p-5">
       <div className="text-center">
