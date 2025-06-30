bd2ab6bb245df0b4a61edddaa9830c6aedf74d27
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-06-30 10:52:58 +0530

    Need to add remaining some fields in the Clients

--- server/controllers/clientController.js
+++ server/controllers/clientController.js
@@ -2,15 +2,7 @@ import Client from "../models/Client.js";
 import Department from "../models/Department.js";
 import Site from "../models/Site.js";
 
-const stateGstCodes = {
-  "Andhra Pradesh": "37", "Arunachal Pradesh": "12", "Assam": "18", "Bihar": "10",
-  "Chhattisgarh": "22", "Delhi": "07", "Goa": "30", "Gujarat": "24", "Haryana": "06",
-  "Himachal Pradesh": "02", "Jammu and Kashmir": "01", "Jharkhand": "20", "Karnataka": "29",
-  "Kerala": "32", "Madhya Pradesh": "23", "Maharashtra": "27", "Manipur": "14", "Meghalaya": "17",
-  "Mizoram": "15", "Nagaland": "13", "Odisha": "21", "Punjab": "03", "Rajasthan": "08", "Sikkim": "11",
-  "Tamil Nadu": "33", "Telangana": "36", "Tripura": "16", "Uttar Pradesh": "09", "Uttarakhand": "05",
-  "West Bengal": "19", "Puducherry": "34", "Other Territory": "97"
-};
+const stateGstCodes = { "Andaman and Nicobar Islands" : "35", "Andhra Pradesh": "37", "Arunachal Pradesh": "12", "Assam": "18", "Bihar": "10", "Chandigarh": "04", "Chhattisgarh": "22", "Dadra and Nagar Haveli" : "26", "Daman and Diu" : "25", "Delhi": "07", "Goa": "30", "Gujarat": "24", "Haryana": "06", "Himachal Pradesh": "02", "Jammu and Kashmir": "01", "Jharkhand": "20", "Karnataka": "29", "Kerala": "32", "Ladakh" : "38", "Lakshadweep" : "31", "Madhya Pradesh": "23", "Maharashtra": "27", "Manipur": "14", "Meghalaya": "17", "Mizoram": "15", "Nagaland": "13", "Odisha": "21", "Other Territory": "97", "Puducherry" : "34", "Punjab": "03", "Rajasthan": "08", "Sikkim": "11", "Tamil Nadu": "33", "Telangana": "36", "Tripura": "16", "Uttar Pradesh": "09", "Uttarakhand": "05", "West Bengal": "19", "Foreign Country": "96" };
 
 const getClients = async (req, res) => {
   try {
