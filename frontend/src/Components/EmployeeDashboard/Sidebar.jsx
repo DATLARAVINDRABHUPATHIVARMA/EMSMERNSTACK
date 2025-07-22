fff57a0dbd5186de6e036bc6cacfc9dce8dfcca2
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-07-22 12:16:34 +0530

    Emp dash update properly after errors

--- frontend/src/Components/EmployeeDashboard/Sidebar.jsx
+++ frontend/src/Components/EmployeeDashboard/Sidebar.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom';
+import { useAuth } from "../../Context/AuthContext.jsx";
 import { FaBuilding, FaCalendarAlt, FaCogs, FaHouseUser, FaMapMarked, FaMoneyBillWave, FaStoreAlt, FaTachometerAlt, FaUsers, } from 'react-icons/fa';
 
 const Sidebar = () => {
+  const {user} = useAuth()
   return (
     <div className='bg-gray-800 text-white h-screen fixed left-0 top-0 space-y-2 w-64'>
       <div className='bg-purple-500 h-12 flex items-center justify-center'>
@@ -13,11 +15,11 @@ const Sidebar = () => {
           <FaTachometerAlt /> 
           <span>Dashboard</span> 
         </NavLink>
-        <NavLink to='/employee-dashboard/profile' className={({isActive}) => `${isActive ? "bg-purple-500" : " "} flex items-center space-x-4 block py-2.5 px-4 rounded`}>
+        <NavLink to={`/employee-dashboard/profile/${user._id}`} className={({isActive}) => `${isActive ? "bg-purple-500" : " "} flex items-center space-x-4 block py-2.5 px-4 rounded`}>
           <FaUsers /> 
           <span>My Profile</span> 
         </NavLink>
-        <NavLink to='/employee-dashboard/leave' className={({isActive}) => `${isActive ? "bg-purple-500" : " "} flex items-center space-x-4 block py-2.5 px-4 rounded`}>
+        <NavLink to='/employee-dashboard/leaves' className={({isActive}) => `${isActive ? "bg-purple-500" : " "} flex items-center space-x-4 block py-2.5 px-4 rounded`}>
           <FaCalendarAlt /> 
           <span>Leave</span> 
         </NavLink>
