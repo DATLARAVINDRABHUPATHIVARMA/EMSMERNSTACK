db32091a7d71b7ef96a2d94965e35d0c06be6793
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-08-20 17:53:27 +0530

    List of Leaves need to fetch SNO err

--- server/routes/leave.js
+++ server/routes/leave.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import authMiddleware from '../middleware/authMiddleware.js';
-import { addLeave } from '../controllers/leaveController.js'
+import { addLeave, getLeaves } from '../controllers/leaveController.js'
 
 const router = express.Router()
 
-// router.get ('/', authMiddleware, getClients)
+router.get ('/:id', authMiddleware, getLeaves)
 router.post ('/add', authMiddleware, addLeave)
 // router.get ('/:id', authMiddleware, getClient)
 // router.put ('/:id', authMiddleware, updateClient)
