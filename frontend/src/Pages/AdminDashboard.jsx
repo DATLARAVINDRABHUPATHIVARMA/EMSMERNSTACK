cedcce158b41dbd7f23777d0bdc84e0904111026
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-08-22 10:43:59 +0530

    work

--- frontend/src/Pages/AdminDashboard.jsx
+++ frontend/src/Pages/AdminDashboard.jsx
@@ -8,7 +8,7 @@ import { Outlet } from "react-router-dom";
 const AdminDashboard = () => { 
   const { user } = useAuth();
 
-  return ( 
+  return (
     <div className="flex">
       <AdminSidebar />
       <div className="flex-1 ml-64 bg-gray-100 h-screen">
