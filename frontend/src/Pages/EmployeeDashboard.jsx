35ad1f2ca0c9d197327a115af7dc2cda91b1e63d
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-09-28 10:23:04 +0530

    EMP DB

--- frontend/src/Pages/EmployeeDashboard.jsx
+++ frontend/src/Pages/EmployeeDashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"; 
 import Sidebar from "../Components/EmployeeDashboard/Sidebar.jsx";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/dashboard/Navbar.jsx";
