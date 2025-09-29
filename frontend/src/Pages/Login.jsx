0bc8370035999529db9fcc194ce188f4a311ebf9
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-09-29 10:21:42 +0530

    context API

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
   const toggleVisibility = () => setVisiblePassword(!visiblePassword);
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault(); 
     try {
       const response = await axios.post(
         "http://localhost:5000/api/auth/login",
