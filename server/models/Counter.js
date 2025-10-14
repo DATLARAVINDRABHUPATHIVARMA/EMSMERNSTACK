5164ef12e9595791aead7ad1f50ce48a9b5ebc1f
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-10-14 16:41:37 +0530

    14th oct backup

--- server/models/Counter.js
+++ server/models/Counter.js
@@ -4,7 +4,7 @@ const CounterSchema = new mongoose.Schema({
   _id: { type: String, required: true },
   seq: { type: Number, default: 83 } // Start from 83 as requested
 });
-
+ 
 const Counter = mongoose.model('Counter', CounterSchema);
 
 export default Counter;
