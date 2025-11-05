5fb5b5c14a3c07488d938b1596cfb8e5ae624484
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-11-05 10:37:25 +0530

    counter

--- server/models/Counter.js
+++ server/models/Counter.js
@@ -1,10 +1,10 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const CounterSchema = new mongoose.Schema({
   _id: { type: String, required: true },
-  seq: { type: Number, default: 83 } // Start from 83 as requested
+  seq: { type: Number, default: 83 }, // Start from 83 as requested
 });
- 
-const Counter = mongoose.model('Counter', CounterSchema);
+
+const Counter = mongoose.model("Counter", CounterSchema);
 
 export default Counter;
